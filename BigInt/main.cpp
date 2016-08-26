ff34ec21e9dcbbffa08c2977edfcdb1886207ea5
Seiji Emery <seijiemery@gmail.com>
2016-08-25 22:07:11 -0700

    added unit tests + fixed missing shift in storeIntParts

--- BigInt/main.cpp
+++ BigInt/main.cpp
@@ -22,16 +22,61 @@ typedef uint64_t bigInt_t;    // BigInt operation type (mul + add ops need 2x pr
     
 constexpr size_t LOW_BITMASK = (1L << STORAGE_BITS) - 1;
 constexpr size_t HIGH_BITMASK = ~LOW_BITMASK;
+    
+UNITTEST_METHOD(verifyStorageValueTypes) {
+    TEST_ASSERT_EQ(STORAGE_BITS, 32, "storage size changed! (tests assume 32-bit");
+    TEST_ASSERT_EQ(sizeof(smallInt_t) * 8, STORAGE_BITS, "storage size does not match STORAGE_BITS!");
+    TEST_ASSERT_EQ(sizeof(smallInt_t) * 2, sizeof(bigInt_t), "big int is not 2x size of small int!");
+    
+    TEST_ASSERT_EQ((smallInt_t)(1L << STORAGE_BITS), 0, "storage size not big enough");
+    TEST_ASSERT_EQ((bigInt_t)(1L << STORAGE_BITS), 1L << STORAGE_BITS, "op size not big enough");
+    TEST_ASSERT_EQ(LOW_BITMASK & HIGH_BITMASK, 0, "LOW_BITMASK overlaps with HIGH_BITMASK");
+    TEST_ASSERT_EQ(LOW_BITMASK | HIGH_BITMASK, ((size_t)0) - 1, "LOW_BITMASK does not have perfect coverage with HIGH_BITMASK");
+} UNITTEST_END_METHOD
 
 bigInt_t fromIntParts (smallInt_t high, smallInt_t low) {
     return ((bigInt_t)high << STORAGE_BITS) | (bigInt_t)low;
 }
+UNITTEST_METHOD(fromIntParts) {
+    
+    TEST_ASSERT_EQ(fromIntParts(0x15, 0x227), 0x1500000227);
+    TEST_ASSERT_EQ(fromIntParts(0x0, (smallInt_t)0xAAAA12847923), 0x12847923);
+    TEST_ASSERT_EQ(fromIntParts(0x0, 0x0), 0x0);
+    TEST_ASSERT_EQ(fromIntParts(0xAA, 0x0), 0xAA00000000);
+    
+} UNITTEST_END_METHOD
     
 void storeIntParts (bigInt_t v, smallInt_t& high, smallInt_t& low) {
-    high = (smallInt_t)(v & HIGH_BITMASK);
+    high = (smallInt_t)((v & HIGH_BITMASK) >> STORAGE_BITS);
     low  = (smallInt_t)(v & LOW_BITMASK);
 }
-                   
+UNITTEST_METHOD(storeIntParts) {
+    smallInt_t a, b;
+    
+    storeIntParts(0x0, a, b);
+    TEST_ASSERT_EQ(a, 0);
+    TEST_ASSERT_EQ(b, 0);
+    
+    storeIntParts(0x1500000227, a, b);
+    TEST_ASSERT_EQ(a, 0x15);
+    TEST_ASSERT_EQ(b, 0x227);
+    
+    storeIntParts(0xAAAA12847923, a, b);
+    TEST_ASSERT_EQ(a, 0xAAAA);
+    TEST_ASSERT_EQ(b, 0x12847923);
+    
+    storeIntParts(0xAA00000000, a, b);
+    TEST_ASSERT_EQ(a, 0xAA);
+    TEST_ASSERT_EQ(b, 0x0);
+
+} UNITTEST_END_METHOD
+    
+UNITTEST_MAIN_METHOD(storage::unittests) {
+    RUN_UNITTEST(verifyStorageValueTypes, UNITTEST_INSTANCE);
+    RUN_UNITTEST(fromIntParts,  UNITTEST_INSTANCE);
+    RUN_UNITTEST(storeIntParts, UNITTEST_INSTANCE);
+} UNITTEST_END_METHOD
+    
 }; // namespace storage
 
 struct typeinfo_t {
@@ -330,15 +375,24 @@ std::ostream& operator << (std::ostream& os, BigInt& v) {
     return os << v.toString();
 }
 
-UNITTEST_METHOD(runAllTests) {
-    RUN_UNITTEST_MAIN(BigInt, UNITTEST_INSTANCE);
-    // more tests to go here...
-    
-} UNITTEST_END_METHOD
+//UNITTEST_METHOD(runAllTests) {
+//    RUN_UNITTEST_MAIN(storage, UNITTEST_INSTANCE);
+//    RUN_UNITTEST_MAIN(BigInt,  UNITTEST_INSTANCE);
+//    // more tests to go here...
+//} UNITTEST_END_METHOD
+
+bool runAllTests () {
+    return
+        RUN_UNITTEST_MAIN(storage) &&
+        RUN_UNITTEST_MAIN(BigInt);
+}
+
 
 int main(int argc, const char * argv[]) {
-    if (!RUN_UNITTEST(runAllTests))
+    if (!runAllTests())
         return -1;
+//    if (!RUN_UNITTEST(runAllTests))
+//        return -1;
     
     auto x = BigInt { "-123456789" };
     auto y = BigInt { "2" };
